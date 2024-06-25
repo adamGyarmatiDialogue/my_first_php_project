6cd92caf39e3acc8f7e0876dbe7242d414376b1a
Adam Gyarmati <adam.gyarmati@dialoguecreatives.com>
2024-06-25 09:34:39 +0200

    add posts fom and PostCreate class. Add PhPUnit to project

---
+++ app/services/posts/CreatePost.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Services\Posts;
+
+final class CreatePost
+{
+}
--- views/pages/admin/posts/create.php
+++ views/pages/admin/posts/create.php
@@ -7,5 +7,35 @@
         <?php require "components/posts-menu.php"; ?>
 
     </div>
-    <div class="page-content"></div>
+    <div class="page-content">
+        <div class="card">
+            <div class="card-body">
+                <form method="POST" action="app/services/posts/create.php" enctype="multipart/form-data">
+                    <div class="mb-2">
+                        <input type="text" class="from-control" placeholder="Bejegyzés címe" name="postTitle">
+                    </div>
+
+                    <div class="mb-2">
+                        <textarea class="from-control" placeholder="Bejegyzés bevezető szöveg" name="postLead"> </textarea>
+                    </div>
+
+                    <div class="mb-2">
+                        <textarea class="from-control" placeholder="Bejegyzés szövege" name="postContent"> </textarea>
+                    </div>
+
+                    <div class="mb-2">
+                        <input type="date" class="from-control" placeholder="Publikálás dátuma" name="postPublishedAt">
+                    </div>
+
+                    <div class="mb-2">
+                        <input type="file" accept="image/*" class="from-control" multiple name="files[]">
+                    </div>
+
+                    <div>
+                        <button type="submit">Létrehozás</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
